8b1b7170c8156df37d4047cc7a604f39a4f9aa96
aquinteror <Aquinteror@intergrupo.com>
2023-12-07 23:45:10 -0500

    UnitTests

--- Tests/CalculatorServiceTests.cs
+++ Tests/CalculatorServiceTests.cs
@@ -8,40 +8,122 @@ namespace Tests
     public class CalculatorServiceTests
     {
         [TestMethod]
-        public void CalculateTotalCost_ShouldCalculateCorrectTotalCost()
+        public void CalculateTotalCost_ValidVehicleCommon_ReturnsAuctionCosts()
         {
             // Arrange
-            var vehicle = new Vehicle { BasePrice = 1000m, VehicleType = "Common" };
-            var calculatorService = new CalculatorService();
+            var vehicle = new Vehicle
+            {
+                VehicleType = "Common",
+                BasePrice = 398m
+            };
+
+            var calculatorService = calculatorServiceTest();
 
             // Act
-            var totalCost = calculatorService.CalculateTotalCost(vehicle);
+            var result = calculatorService.CalculateTotalCost(vehicle);
 
             // Assert
-            Assert.AreEqual(1180m, totalCost);
-        }
-
-        public void CalculateTotalCost_ShouldCalculateCorrectTotalCost(decimal basePrice, string vehicleType, decimal expectedTotalCost)
-        {
-            // Arrange
-            var vehicle = new Vehicle { BasePrice = basePrice, VehicleType = vehicleType };
-            var calculatorService = new CalculatorService();
-
-            // Act
-            var totalCost = calculatorService.CalculateTotalCost(vehicle);
-
-            // Assert
-            Assert.AreEqual(expectedTotalCost, totalCost);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(550.76m, result.TotalCost);
         }
 
         [TestMethod]
-        public void CalculateTotalCost_ShouldThrowArgumentNullException_WhenVehicleIsNull()
+        public void CalculateTotalCost_ValidVehicleCommonTest2_ReturnsAuctionCosts()
+        {
+            // Arrange
+            var vehicle = new Vehicle
+            {
+                VehicleType = "Common",
+                BasePrice = 501m
+            };
+
+            var calculatorService = calculatorServiceTest();
+
+            // Act
+            var result = calculatorService.CalculateTotalCost(vehicle);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(671.02m, result.TotalCost);
+        }
+
+        [TestMethod]
+        public void CalculateTotalCost_ValidVehicleCommonTest3_ReturnsAuctionCosts()
+        {
+            // Arrange
+            var vehicle = new Vehicle
+            {
+                VehicleType = "Common",
+                BasePrice = 57m
+            };
+
+            var calculatorService = calculatorServiceTest();
+
+            // Act
+            var result = calculatorService.CalculateTotalCost(vehicle);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(173.14m, result.TotalCost);
+        }
+
+        [TestMethod]
+        public void CalculateTotalCost_ValidVehicleLuxury_ReturnsAuctionCosts()
+        {
+            // Arrange
+            var vehicle = new Vehicle
+            {
+                VehicleType = "Luxury",
+                BasePrice = 1800 // 
+            };
+
+            var calculatorService = calculatorServiceTest();
+
+            // Act
+            var result = calculatorService.CalculateTotalCost(vehicle);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2167m, result.TotalCost);
+        }
+
+        [TestMethod]
+        public void CalculateTotalCost_ValidVehicleLuxuryTest2_ReturnsAuctionCosts()
+        {
+            // Arrange
+            var vehicle = new Vehicle
+            {
+                VehicleType = "Luxury",
+                BasePrice = 1000000 // 
+            };
+
+            var calculatorService = calculatorServiceTest();
+
+            // Act
+            var result = calculatorService.CalculateTotalCost(vehicle);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1040320m, result.TotalCost);
+        }
+
+        [TestMethod]
+        public void CalculateTotalCost_NullVehicle_ThrowsArgumentNullException()
         {
             // Arrange
             var calculatorService = new CalculatorService();
 
-            // Act & Assert
-            Assert.ThrowsException<ArgumentNullException>(() => calculatorService.CalculateTotalCost(null));
+            // Act and Assert
+            Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                calculatorService.CalculateTotalCost(null!);
+            });
+
+        }
+
+        public CalculatorService calculatorServiceTest()
+        {
+            return new CalculatorService();
         }
     }
 }
---
+++ Tests/ControllerCalculatorServiceTests.cs
@@ -0,0 +1,89 @@
+﻿using Api.Models;
+using AutoMapper;
+using Business.Interfaces;
+using Controller.Controllers;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class ControllerCalculatorServiceTests
+    {
+        [TestMethod]
+        public void CalculateTotalCost_ValidRequest_ReturnsOkResult()
+        {
+            // Arrange
+            var calculatorServiceMock = new Mock<ICalculatorSercive>();
+            var mapperMock = new Mock<IMapper>();
+
+            var controller = new VehicleController(calculatorServiceMock.Object, mapperMock.Object);
+
+            var vehicleRequest = new VehicleRequest(398,"Common");
+
+            var vehicle = new Vehicle
+            {
+                BasePrice = 398,
+                VehicleType = "Common"
+            };
+
+            var auctionCosts = new AuctionCosts
+            {
+                BasicUserFee = 100,
+                AssociationFee = 10,
+                SellersSpecialFee = 30,
+                StorageFee = 10,
+                TotalCost = 500
+            };
+
+            var auctionCostsResponse = new AuctionCostsResponse
+            {
+                BasicUserFee = 100,
+                AssociationFee = 10,
+                SellersSpecialFee = 30,
+                StorageFee = 10,
+                TotalCost = 500
+            };
+
+            mapperMock.Setup(m => m.Map<Vehicle>(It.IsAny<VehicleRequest>())).Returns(vehicle);
+            calculatorServiceMock.Setup(c => c.CalculateTotalCost(It.IsAny<Vehicle>())).Returns(auctionCosts);
+            mapperMock.Setup(m => m.Map<AuctionCostsResponse>(It.IsAny<AuctionCosts>())).Returns(auctionCostsResponse);
+
+            // Act
+            var result = controller.CalculateTotalCost(vehicleRequest);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = (OkObjectResult)result;
+            Assert.IsInstanceOfType(okResult.Value, typeof(AuctionCostsResponse));
+            var resultValue = (AuctionCostsResponse)okResult.Value!;
+
+            Assert.IsNotNull(resultValue);
+        }
+
+        [TestMethod]
+        public void CalculateTotalCost_InvalidRequest_ReturnsBadRequest()
+        {
+            // Arrange
+            var calculatorServiceMock = new Mock<ICalculatorSercive>();
+            var mapperMock = new Mock<IMapper>();
+
+            var controller = new VehicleController(calculatorServiceMock.Object, mapperMock.Object);
+
+            // Act
+            var result = controller.CalculateTotalCost(null!);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+            var badRequestResult = (BadRequestObjectResult)result;
+            Assert.AreEqual("Invalid vehicle data.", badRequestResult.Value);
+        }
+    }
+}
